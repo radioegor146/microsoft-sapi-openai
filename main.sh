5f4013a168f7d812065ea918577838f0012ddacf
Egor Koleda <radioegor146@ctf.by>
2025-07-06 17:26:40 +0300

    fix

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ -f /home/wineuser/voices-ready ]; then
+if [ ! -f /home/wineuser/voices-ready ]; then
     wine reg delete "HKLM\\Software\\Microsoft\\Speech\\Voices\\Tokens\\Wine Default Voice" /f
     mkdir /tmp/voices
     wget https://microsoft-sapi-openai-voices.re146.dev/s2g-tatyana-x86.exe -O /tmp/voices/s2g-tatyana-x86.exe && wine /tmp/voices/s2g-tatyana-x86.exe /S /NCRC
