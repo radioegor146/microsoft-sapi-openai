63affc54eb620e9a08513ee0daaafcb4a98c8c8f
Egor Koleda <radioegor146@ctf.by>
2025-07-18 03:20:03 +0300

    add multiple voices support and add kendra s2g voice

--- SpeechAPITTS/Program.cs
+++ SpeechAPITTS/Program.cs
@@ -8,6 +8,8 @@ using System.Reflection;
 using System.Speech.AudioFormat;
 using System.Speech.Synthesis;
 using System.Text;
+using System.Text.RegularExpressions;
+using static SpeechAPITTS.OpenAIVoicesResponse;
 
 namespace SpeechAPITTS
 {
@@ -45,6 +47,27 @@ namespace SpeechAPITTS
 
         [JsonProperty("response_format", Required = Required.Always)]
         public string ResponseFormat { get; set; } = string.Empty;
+    }
+
+    internal class VoiceWithCulture
+    {
+        [JsonProperty("voice", Required = Required.Always)]
+        public string Voice { get; set; } = string.Empty;
+
+        [JsonProperty("culture", Required = Required.Always)]
+        public string Culture { get; set; } = string.Empty;
+
+        [JsonProperty("triggerRegexp", Required = Required.Always)]
+        public string TriggerRegexp { get; set; } = string.Empty;
+    }
+
+    internal class ExtendedVoiceRequest
+    {
+        [JsonProperty("voices", Required = Required.Always)]
+        public List<VoiceWithCulture> Voices = [];
+
+        [JsonProperty("defaultCulture", Required = Required.Always)]
+        public string DefaultCulture = string.Empty;
     }
 
     internal interface IFormatConverter
@@ -163,44 +186,19 @@ namespace SpeechAPITTS
                 return;
             }
 
-            var synthesizer = new SpeechSynthesizer();
-            FixVoices(synthesizer);
-
-            var voices = synthesizer.GetInstalledVoices();
-
-            var voice = voices.FirstOrDefault(otherVoice =>
+            var extendedVoiceRequest = TryDecodeExtendedVoiceRequest(request.Voice);
+            byte[]? sampleData;
+            if (extendedVoiceRequest != null)
             {
-                Debug.Assert(OperatingSystem.IsWindows());
-                return otherVoice.Enabled && otherVoice.VoiceInfo.Name == request.Voice;
-            });
-
-            if (voice == null)
+                sampleData = await SpeakUsingExtendedVoice(context, request.Input, extendedVoiceRequest);
+            } 
+            else
             {
-                await SendJsonResponse(context, 400, new OpenAIErrorResponse
-                {
-                    Error = "malformed body",
-                    Details = $"voice '{request.Voice}' not found"
-                });
-                return;
+                sampleData = await SpeakUsingVoice(context, request.Input, request.Voice);
             }
 
-            var outputStream = new MemoryStream();
-
-            try
+            if (sampleData == null)
             {
-                synthesizer.SelectVoice(voice.VoiceInfo.Name);
-                synthesizer.SetOutputToAudioStream(outputStream,
-                    new SpeechAudioFormatInfo(SampleRate, AudioBitsPerSample.Sixteen, Channels == 1 ? AudioChannel.Mono :
-                    Channels == 2 ? AudioChannel.Stereo : throw new Exception("wrong number of channels")));
-                synthesizer.Speak(request.Input);
-            }
-            catch (Exception ex)
-            {
-                await SendJsonResponse(context, 500, new OpenAIErrorResponse
-                {
-                    Error = "internal error",
-                    Details = $"failed to speak: {ex}"
-                });
                 return;
             }
 
@@ -208,7 +206,7 @@ namespace SpeechAPITTS
 
             try
             {
-                var input = outputStream.ToArray();
+                var input = sampleData;
                 var samples = new short[input.Length / 2];
                 Buffer.BlockCopy(input, 0, samples, 0, input.Length);
 
@@ -228,6 +226,161 @@ namespace SpeechAPITTS
             context.Response.StatusCode = 200;
             await context.Response.OutputStream.WriteAsync(converted);
             context.Response.Close();
+        }
+
+        private static async Task<byte[]?> SpeakUsingVoice(IHttpContext context, string input, string voiceName)
+        {
+            Debug.Assert(OperatingSystem.IsWindows());
+
+            var synthesizer = new SpeechSynthesizer();
+            FixVoices(synthesizer);
+
+            var voices = synthesizer.GetInstalledVoices();
+
+            var voice = voices.FirstOrDefault(otherVoice =>
+            {
+                Debug.Assert(OperatingSystem.IsWindows());
+                return otherVoice.Enabled && otherVoice.VoiceInfo.Name == voiceName;
+            });
+
+            if (voice == null)
+            {
+                await SendJsonResponse(context, 400, new OpenAIErrorResponse
+                {
+                    Error = "malformed body",
+                    Details = $"voice '{voiceName}' not found"
+                });
+                return null;
+            }
+
+            var outputStream = new MemoryStream();
+
+            try
+            {
+                synthesizer.SelectVoice(voice.VoiceInfo.Name);
+                synthesizer.SetOutputToAudioStream(outputStream,
+                    new SpeechAudioFormatInfo(SampleRate, AudioBitsPerSample.Sixteen, Channels == 1 ? AudioChannel.Mono :
+                    Channels == 2 ? AudioChannel.Stereo : throw new Exception("wrong number of channels")));
+                synthesizer.Speak(input);
+            }
+            catch (Exception ex)
+            {
+                await SendJsonResponse(context, 500, new OpenAIErrorResponse
+                {
+                    Error = "internal error",
+                    Details = $"failed to speak: {ex}"
+                });
+                return null;
+            }
+
+            return outputStream.ToArray();
+        }
+
+        private static async Task<byte[]?> SpeakUsingExtendedVoice(IHttpContext context, string input, 
+            ExtendedVoiceRequest extendedVoiceRequest)
+        {
+            Debug.Assert(OperatingSystem.IsWindows());
+
+            var synthesizer = new SpeechSynthesizer();
+            FixVoices(synthesizer);
+
+            var voices = synthesizer.GetInstalledVoices();
+
+            var usedVoices = new Dictionary<string, (Regex Regex, string Voice)>();
+            foreach (var voice in extendedVoiceRequest.Voices)
+            {
+                var voiceInfo = voices.FirstOrDefault(otherVoice =>
+                {
+                    Debug.Assert(OperatingSystem.IsWindows());
+                    return otherVoice.Enabled && otherVoice.VoiceInfo.Name == voice.Voice;
+                });
+
+                if (voiceInfo == null)
+                {
+                    await SendJsonResponse(context, 400, new OpenAIErrorResponse
+                    {
+                        Error = "malformed body",
+                        Details = $"voice '{voice.Voice}' not found"
+                    });
+                    return null;
+                }
+
+                usedVoices.Add(voice.Culture, (new Regex(voice.TriggerRegexp, RegexOptions.CultureInvariant), voice.Voice));
+            }
+
+            var parts = new List<(string Culture, string Part)>();
+
+            var currentStringBuilder = new StringBuilder();
+
+            var currentCulture = extendedVoiceRequest.DefaultCulture;
+
+            foreach (var character in input)
+            {
+                foreach (var culture in usedVoices)
+                {
+                    if (culture.Value.Regex.IsMatch(character.ToString()))
+                    {
+                        if (currentCulture != culture.Key)
+                        {
+                            if (currentStringBuilder.Length > 0)
+                            {
+                                parts.Add((currentCulture, currentStringBuilder.ToString()));
+                                currentStringBuilder.Clear();
+                            }
+                        }
+                        currentCulture = culture.Key;
+                        break;
+                    }
+                }
+
+                currentStringBuilder.Append(character);
+            }
+
+            if (currentStringBuilder.Length > 0)
+            {
+                parts.Add((currentCulture, currentStringBuilder.ToString()));
+            }
+
+            var outputStream = new MemoryStream();
+
+            try
+            {
+                synthesizer.SetOutputToAudioStream(outputStream,
+                    new SpeechAudioFormatInfo(SampleRate, AudioBitsPerSample.Sixteen, Channels == 1 ? AudioChannel.Mono :
+                    Channels == 2 ? AudioChannel.Stereo : throw new Exception("wrong number of channels")));
+                foreach (var part in parts)
+                {
+                    synthesizer.SelectVoice(usedVoices[part.Culture].Voice);
+                    synthesizer.Speak(part.Part);
+                }
+            }
+            catch (Exception ex)
+            {
+                await SendJsonResponse(context, 500, new OpenAIErrorResponse
+                {
+                    Error = "internal error",
+                    Details = $"failed to speak: {ex}"
+                });
+                return null;
+            }
+
+            return outputStream.ToArray();
+        }
+
+        private static ExtendedVoiceRequest? TryDecodeExtendedVoiceRequest(string voice)
+        {
+            if (!voice.StartsWith('{'))
+            {
+                return null;
+            }
+            try
+            {
+                return JsonConvert.DeserializeObject<ExtendedVoiceRequest>(voice);
+            }
+            catch
+            {
+                return null;
+            }
         }
 
         private static void FixVoices(SpeechSynthesizer synthesizer)
--- main.sh
+++ main.sh
@@ -4,6 +4,7 @@ if [ ! -f /home/wineuser/voices-ready ]; then
     mkdir /tmp/voices
     wget https://microsoft-sapi-openai-voices.re146.dev/s2g-tatyana-x86.exe -O /tmp/voices/s2g-tatyana-x86.exe && wine /tmp/voices/s2g-tatyana-x86.exe /S /NCRC
     wget https://microsoft-sapi-openai-voices.re146.dev/s2g-maxim-x86.exe -O /tmp/voices/s2g-maxim-x86.exe && wine /tmp/voices/s2g-maxim-x86.exe /S /NCRC
+    wget https://microsoft-sapi-openai-voices.re146.dev/s2g-kendra-x86.exe -O /tmp/voices/s2g-kendra-x86.exe && wine /tmp/voices/s2g-kendra-x86.exe /S /NCRC
     rm -rf /tmp/voices/
     touch /home/wineuser/voices-ready
 fi
